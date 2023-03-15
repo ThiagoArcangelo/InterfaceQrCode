eb88b644d932addf7e2d79156715f1f80de10476
Thiago <devtsarcangelo@gmail.com>
2023-03-14 22:18:11 -0300

    new logo

--- src/components/HeaderMain/index.jsx
+++ src/components/HeaderMain/index.jsx
@@ -1,14 +1,14 @@
 /* eslint-disable jsx-a11y/alt-text */
 import "./style.css";
 import { Link } from "react-router-dom";
-import logo from "../../assets/3.png";
+import qrlogo from "../../assets/qrlogo.jpg";
 
 const HeaderMain = () => {
   return (
     <div className="header-content">
       <header>        
         <Link to="/">
-          <img src={logo} className='imag-header' />
+          <img src={qrlogo} className='imag-header' />
         </Link> 
         <Link to="/generator">Gerar QrCode</Link>         
       </header>
